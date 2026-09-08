994e4814c9506394bb347c070d9b354da65edeba
kos-369 <cn.8rlms8@gmail.com>
2026-09-08 21:34:16 +0900

    add property based tests

--- roman-numerals/roman-numerals_test.go
+++ roman-numerals/roman-numerals_test.go
@@ -2,11 +2,14 @@ package roman_numerals
 
 import (
 	"fmt"
+	"math/rand"
+	"reflect"
 	"testing"
+	"testing/quick"
 )
 
 var cases = []struct {
-	Arabic int
+	Arabic uint16
 	Roman  string
 }{
 	{Arabic: 1, Roman: "I"},
@@ -59,5 +62,25 @@ func TestConvertingToArabic(t *testing.T) {
 				t.Errorf("got %d, want %d", got, test.Arabic)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		t.Log("testing", arabic)
+
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, &quick.Config{
+		MaxCount: 1000,
+		Values: func(args []reflect.Value, r *rand.Rand) {
+			args[0] = reflect.ValueOf(uint16(r.Intn(4000)))
+		},
+	}); err != nil {
+		t.Error("failed checks", err)
 	}
 }
